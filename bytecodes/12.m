0f29470e73f02a277d8b789c5269c940a656a1e0
chalwemwansa <chalwemwansa23@gmail.com>
2023-09-22 14:35:57 +0200

    probably the final commit to the project for monty stacka nd queue

--- bytecodes/12.m
+++ bytecodes/12.m
@@ -1,20 +1,2 @@
-	pall
-push 4
-			
-pint		6 
-push 7
-		  push 9
-		  	add 5
-			pall 
-		push 10
-	nop 3
-	push 7
-add 5
-pall
-	swap 4||$
-	add 
-kay		
-			pall $
-		add
-			add
-	pall
+push 1
+pope
