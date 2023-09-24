6066450e53d88a7f6579fd97a478589078ea15a4
Shimapondo <williamnamabende@gmail.com>
2023-09-24 13:06:32 -0700

    somemore commit

--- length.c
+++ length.c
@@ -1,11 +1,12 @@
 #include "monty.h"
 
 /**
- * _len - finds the length of a string
+ * _len - function finds the length of a string
  *
  * @str: the parameter used
  * Return: length of a string
  */
+
 int _len(char *str)
 {
 	int length = 0;
